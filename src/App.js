a6151c83fc032fd8f74098aa4c6aa8b8b3010b08
Ronald I. Velarde <ronv833@example.com>
2021-12-02 18:18:34 +0800

    Creating a react app for Daily Schedule Lists

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
+import Mytask from './Mytask';
 import './App.css';
+import Mythoughts from './Mythoughts';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1 style={{marginTop:'0', padding:'3%'}}>Sample of my Daily Schedules</h1>
+        <Mytask/>
+        <Mythoughts/>
     </div>
   );
 }
